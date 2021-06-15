31d2ba5778af9981e6251ab9b5404c5819a0b1bc
nducthang <nguyenducthang8a2@gmail.com>
2021-06-15 20:32:14 +0700

    MMAXPER

--- VNOI/MMAXPER.cpp
+++ VNOI/MMAXPER.cpp
@@ -1,27 +1,20 @@
 #include <iostream>
 #include <cmath>
+#include <algorithm>
 using namespace std;
-#define MAX 1001
-int n,a[MAX],b[MAX],f[MAX],g[MAX];
-/*
-n la so hinh chu nhat
-a[i] la chieu rong hinh chu nhat i
-b[i] la chieu dai hinh chu nhat i
-f[i] la truong hop hcn i nam ngang
-g[i] la truong hop hcn i nam thang dung
-*/
-
+#define MAX 1002
+int n, x[MAX], y[MAX];
+int f[MAX], g[MAX];
 int main()
 {
-	cin>>n;
-	for(int i=1;i<=n;i++)
-		cin>>a[i]>>b[i];
-	f[1]=b[1];
-	g[1]=a[1];
-	for(int i=2;i<=n;i++)
-	{
-		f[i]=max(f[i-1]+b[i]+abs(a[i-1]-a[i]),g[i-1]+b[i]+abs(b[i-1]-a[i]));
-		g[i]=max(g[i-1]+a[i]+abs(b[i-1]-b[i]),f[i-1]+a[i]+abs(a[i-1]-b[i]));
-	}
-	cout<<max(f[n],g[n])<<endl;	
+    cin>>n;
+    for(int i=1;i<=n;i++) cin>>x[i]>>y[i];
+    f[1] = x[1];
+    g[1] = y[1];
+    for(int i=2;i<=n;i++)
+    {
+        f[i] = max(f[i-1] + abs(y[i]-y[i-1]), g[i-1] +abs(y[i]-x[i-1])) + x[i];
+        g[i] = max(f[i-1] + abs(x[i]-y[i-1]), g[i-1] + abs(x[i]-x[i-1])) + y[i];
+    }
+    cout<<max(f[n], g[n]);
 }
